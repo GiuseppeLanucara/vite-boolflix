0cae393eb3a3b98083c1bc2f0d950c7572a437a4
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-11-14 16:03:51 +0100

    creazione componenti vuoti e store.js

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
 import axios from "axios"
 export default {
   created() {
-    axios.get("")
+    axios.get("https://api.themoviedb.org/3/search/movie?api_key=2e302d29775f4de190d7658d34f4d6d2&query=hello")
+      .then((resp) => console.log(resp))
   }
 }
 </script>
--- src/store.js
+++ src/store.js
@@ -3,7 +3,9 @@ export const store = reactive({
     searchStatus: "",
     searchKey: "",
     loading: false,
-    urlMovie: "https://api.themoviedb.org/3/search/movie?",
-    urlSeries: "https://api.themoviedb.org/3/search/tv?",
-    urlKey: "2e302d29775f4de190d7658d34f4d6d2"
+    apiUrlMovie: "https://api.themoviedb.org/3/search/movie?",
+    apiUrlSeries: "https://api.themoviedb.org/3/search/tv?",
+    apiUrlKey: "2e302d29775f4de190d7658d34f4d6d2",
+    movies: [],
+    series: []
 })
