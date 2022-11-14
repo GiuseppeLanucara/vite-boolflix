09ff64da81bba7672d9551c2aa2fa442afcc7d4d
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-11-14 16:28:01 +0100

    fix axios

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ export default {
     }
   },
   created() {
-    axios.get("https://api.themoviedb.org/3/search/movie?api_key=2e302d29775f4de190d7658d34f4d6d2&query=hello")
+    axios.get("https://api.themoviedb.org/3/search/movie?api_key=2e302d29775f4de190d7658d34f4d6d2")
       .then((resp) => console.log(resp))
   }
 }
