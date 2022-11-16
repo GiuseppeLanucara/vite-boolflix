4f36ea1215f8ecd769571fd476c9b25def3d9f81
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-11-16 16:52:38 +0100

    app back e front card

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -25,18 +25,23 @@ export default {
 
             <div class="card" v-for="(movie, index) in store.movies" :key="index">
                 <div class="col">
-                    <img v-if="movie.poster_path" :src="`${store.imgDownload}${movie.poster_path}`">
-                    <img v-else src="../assets/img/no-img.jpg" alt="">
-                    <h2>{{ movie.title }}</h2>
-                    <h3>{{ movie.original_title }}</h3>
-
-                    <div>
-                        <img v-if="avaibleFlags.includes(movie.original_language)"
-                            :src="getImgUrl(movie.original_language)">
-                        <p v-else>{{ movie.original_language }}</p>
+                    <div class="card-front">
+                        <img v-if="movie.poster_path" :src="`${store.imgDownload}${movie.poster_path}`">
+                        <img v-else src="../assets/img/no-img.jpg" alt="">
                     </div>
-                    <span><i v-for="number in Math.ceil(movie.vote_average / 2)"
-                            class="fa-solid fa-star yell"></i></span>
+                    <div class="card-back">
+                        <h2>{{ movie.title }}</h2>
+                        <h3>{{ movie.original_title }}</h3>
+
+                        <div>
+                            <img v-if="avaibleFlags.includes(movie.original_language)"
+                                :src="getImgUrl(movie.original_language)">
+                            <p v-else>{{ movie.original_language }}</p>
+                        </div>
+                        <span><i v-for="number in Math.ceil(movie.vote_average / 2)"
+                                class="fa-solid fa-star yell"></i></span>
+                    </div>
+
                 </div>
             </div>
 
@@ -51,18 +56,23 @@ export default {
 
                 <div class="card" v-for="(series, index) in store.series" :key="index">
                     <div class="col">
-                        <img v-if="series.poster_path" :src="`${store.imgDownload}${series.poster_path}`">
-                        <img v-else src="../assets/img/no-img.jpg" alt="">
-                        <h2>{{ series.name }}</h2>
-                        <h3>{{ series.original_name }}</h3>
+                        <card-front>
+                            <img v-if="series.poster_path" :src="`${store.imgDownload}${series.poster_path}`">
+                            <img v-else src="../assets/img/no-img.jpg" alt="">
+                        </card-front>
+                        <div class="card-back">
+                            <h2>{{ series.name }}</h2>
+                            <h3>{{ series.original_name }}</h3>
 
-                        <div>
-                            <img v-if="avaibleFlags.includes(series.original_language)"
-                                :src="getImgUrl(series.original_language)">
-                            <p v-else>{{ series.original_language }}</p>
+                            <div>
+                                <img v-if="avaibleFlags.includes(series.original_language)"
+                                    :src="getImgUrl(series.original_language)">
+                                <p v-else>{{ series.original_language }}</p>
+                            </div>
+                            <span><i v-for="number in Math.ceil(series.vote_average / 2)"
+                                    class="fa-solid fa-star yell"></i></span>
                         </div>
-                        <span><i v-for="number in Math.ceil(series.vote_average / 2)"
-                                class="fa-solid fa-star yell"></i></span>
+
                     </div>
 
                 </div>
@@ -98,5 +108,30 @@ export default {
     background-color: grey;
     overflow-y: auto;
 
+
+}
+
+.row {
+    margin: 0 auto;
+}
+
+.card {
+    position: relative;
+    height: 100%;
+}
+
+.card-back {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+}
+
+.card-front {
+    height: 100%;
+}
+
+img {
+    height: 100%;
 }
 </style>
