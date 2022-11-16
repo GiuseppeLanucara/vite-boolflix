bbae7d29097355dfa34b75ea384d9da6dc063924
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-11-16 17:26:03 +0100

    fix layout

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -23,26 +23,26 @@ export default {
         <h3>Movies</h3>
         <div class="row">
 
-            <div class="card" v-for="(movie, index) in store.movies" :key="index">
-                <div class="col">
-                    <div class="card-front">
-                        <img v-if="movie.poster_path" :src="`${store.imgDownload}${movie.poster_path}`">
-                        <img v-else src="../assets/img/no-img.jpg" alt="">
-                    </div>
-                    <div class="card-back">
-                        <h2>{{ movie.title }}</h2>
-                        <h3>{{ movie.original_title }}</h3>
+            <div class="col card" v-for="(movie, index) in store.movies" :key="index">
 
-                        <div>
-                            <img v-if="avaibleFlags.includes(movie.original_language)"
-                                :src="getImgUrl(movie.original_language)">
-                            <p v-else>{{ movie.original_language }}</p>
-                        </div>
-                        <span><i v-for="number in Math.ceil(movie.vote_average / 2)"
-                                class="fa-solid fa-star yell"></i></span>
-                    </div>
-
+                <div class="card-front">
+                    <img v-if="movie.poster_path" :src="`${store.imgDownload}${movie.poster_path}`">
+                    <img v-else src="../assets/img/no-img.jpg" alt="">
                 </div>
+                <div class="card-back">
+                    <h2>{{ movie.title }}</h2>
+                    <h3>{{ movie.original_title }}</h3>
+
+                    <div>
+                        <img class="flag" v-if="avaibleFlags.includes(movie.original_language)"
+                            :src="getImgUrl(movie.original_language)">
+                        <p v-else>{{ movie.original_language }}</p>
+                    </div>
+                    <span><i v-for="number in Math.ceil(movie.vote_average / 2)"
+                            class="fa-solid fa-star yell"></i></span>
+                </div>
+
+
             </div>
 
 
@@ -54,26 +54,26 @@ export default {
         <div class="section">
             <div class="row">
 
-                <div class="card" v-for="(series, index) in store.series" :key="index">
-                    <div class="col">
-                        <card-front>
-                            <img v-if="series.poster_path" :src="`${store.imgDownload}${series.poster_path}`">
-                            <img v-else src="../assets/img/no-img.jpg" alt="">
-                        </card-front>
-                        <div class="card-back">
-                            <h2>{{ series.name }}</h2>
-                            <h3>{{ series.original_name }}</h3>
+                <div class="col card" v-for="(series, index) in store.series" :key="index">
 
-                            <div>
-                                <img v-if="avaibleFlags.includes(series.original_language)"
-                                    :src="getImgUrl(series.original_language)">
-                                <p v-else>{{ series.original_language }}</p>
-                            </div>
-                            <span><i v-for="number in Math.ceil(series.vote_average / 2)"
-                                    class="fa-solid fa-star yell"></i></span>
-                        </div>
-
+                    <div class="card-front">
+                        <img v-if="series.poster_path" :src="`${store.imgDownload}${series.poster_path}`">
+                        <img v-else src="../assets/img/no-img.jpg" alt="">
                     </div>
+                    <div class="card-back">
+                        <h2>{{ series.name }}</h2>
+                        <h3>{{ series.original_name }}</h3>
+
+                        <div>
+                            <img class="flag" v-if="avaibleFlags.includes(series.original_language)"
+                                :src="getImgUrl(series.original_language)">
+                            <p v-else>{{ series.original_language }}</p>
+                        </div>
+                        <span><i v-for="number in Math.ceil(series.vote_average / 2)"
+                                class="fa-solid fa-star yell"></i></span>
+                    </div>
+
+
 
                 </div>
 
@@ -99,6 +99,8 @@ export default {
 
 .yell {
     color: yellow;
+    margin-left: 15px;
+    margin-top: 15px;
 }
 
 .container {
@@ -117,7 +119,9 @@ export default {
 
 .card {
     position: relative;
-    height: 100%;
+    height: 50vh;
+    overflow: hidden;
+
 }
 
 .card-back {
@@ -125,13 +129,43 @@ export default {
     top: 0;
     left: 0;
     height: 100%;
+    background-color: rgba(#000000, .8);
+    width: 100%;
+    display: none;
+
+    &:hover {
+        display: block;
+    }
 }
 
 .card-front {
     height: 100%;
+
 }
 
 img {
     height: 100%;
+    width: 100%;
+}
+
+h2 {
+    margin-left: 15px;
+    font-size: xx-small;
+    color: white;
+    margin-top: 5px;
+    margin-bottom: 5px;
+}
+
+h3 {
+    margin-left: 15px;
+    font-size: small;
+    color: white;
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
+
+.flag {
+    width: 150px;
+    margin-left: 15px;
 }
 </style>
