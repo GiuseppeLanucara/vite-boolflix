f90d8d5ee3c5dcb1729971025920046a598e1867
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-11-16 16:09:38 +0100

    col

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -22,18 +22,22 @@ export default {
     <div class="container">
         <h3>Movies</h3>
         <div class="row">
-            <div class="card" v-for="(movie, index) in store.movies" :key="index">
-                <img v-if="movie.poster_path" :src="`${store.imgDownload}${movie.poster_path}`">
-                <img v-else src="../assets/img/no-img.jpg" alt="">
-                <h2>{{ movie.title }}</h2>
-                <h3>{{ movie.original_title }}</h3>
+            <div class="col">
+                <div class="card" v-for="(movie, index) in store.movies" :key="index">
+                    <img v-if="movie.poster_path" :src="`${store.imgDownload}${movie.poster_path}`">
+                    <img v-else src="../assets/img/no-img.jpg" alt="">
+                    <h2>{{ movie.title }}</h2>
+                    <h3>{{ movie.original_title }}</h3>
 
-                <div>
-                    <img v-if="avaibleFlags.includes(movie.original_language)"
-                        :src="getImgUrl(movie.original_language)">
-                    <p v-else>{{ movie.original_language }}</p>
+                    <div>
+                        <img v-if="avaibleFlags.includes(movie.original_language)"
+                            :src="getImgUrl(movie.original_language)">
+                        <p v-else>{{ movie.original_language }}</p>
+                    </div>
+                    <span><i v-for="number in Math.ceil(movie.vote_average / 2)"
+                            class="fa-solid fa-star yell"></i></span>
                 </div>
-                <span><i v-for="number in Math.ceil(movie.vote_average / 2)" class="fa-solid fa-star yell"></i></span>
+
             </div>
 
         </div>
@@ -42,21 +46,24 @@ export default {
         <h2>Series</h2>
         <div class="section">
             <div class="row">
-                <div class="card" v-for="(series, index) in store.series" :key="index">
-                    <img v-if="series.poster_path" :src="`${store.imgDownload}${series.poster_path}`">
-                    <img v-else src="../assets/img/no-img.jpg" alt="">
-                    <h2>{{ series.name }}</h2>
-                    <h3>{{ series.original_name }}</h3>
+                <div class="col">
+                    <div class="card" v-for="(series, index) in store.series" :key="index">
+                        <img v-if="series.poster_path" :src="`${store.imgDownload}${series.poster_path}`">
+                        <img v-else src="../assets/img/no-img.jpg" alt="">
+                        <h2>{{ series.name }}</h2>
+                        <h3>{{ series.original_name }}</h3>
 
-                    <div>
-                        <img v-if="avaibleFlags.includes(series.original_language)"
-                            :src="getImgUrl(series.original_language)">
-                        <p v-else>{{ series.original_language }}</p>
+                        <div>
+                            <img v-if="avaibleFlags.includes(series.original_language)"
+                                :src="getImgUrl(series.original_language)">
+                            <p v-else>{{ series.original_language }}</p>
+                        </div>
+                        <span><i v-for="number in Math.ceil(series.vote_average / 2)"
+                                class="fa-solid fa-star yell"></i></span>
+
                     </div>
-                    <span><i v-for="number in Math.ceil(series.vote_average / 2)"
-                            class="fa-solid fa-star yell"></i></span>
-
                 </div>
+
             </div>
 
         </div>
